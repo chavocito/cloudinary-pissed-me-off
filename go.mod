be645463941515fba7c333b4acc91c3bd6f1011d
chavocito <fiifiowusu@aol.com>
2024-08-20 16:17:05 +0000

    w.i.p: converting this sh### to a cli app

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/creasty/defaults v1.7.0 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/gorilla/schema v1.4.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,1 +1,58 @@
-package cloudinary
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/cloudinary/cloudinary-go/v2/api/admin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load("./.env")
+	if err != nil {
+		log.Fatal("Error loading .env files")
+	}
+
+	if err != nil {
+		fmt.Println("Error instantiating cloudinary client")
+		return
+	}
+}
+
+func buildCloudinaryClient(apiKey string, apiSecret string, cloudname string) (*cloudinary.Cloudinary, error) {
+	cloudinaryUrl := fmt.Sprintf("cloudinary://%s:%s@%s", apiKey, apiSecret, cloudname)
+	return cloudinary.NewFromURL(cloudinaryUrl)
+}
+
+func deleteAllAssets(tag string, cld *cloudinary.Cloudinary) {
+	resp, err := cld.Admin.DeleteAssetsByTag(context.Background(), admin.DeleteAssetsByTagParams{
+		Tag: tag,
+	})
+
+	if err != nil {
+		fmt.Println("Error deleting assets")
+		return
+	}
+
+	fmt.Println(resp.Deleted)
+}
+
+func deleteDirectory() {
+	// Initialize the Cloudinary client
+	cld, err := cloudinary.NewFromURL("cloudinary://:4oD7U--nIASP1GeTlU86Fdyu_sE@ebrayce")
+	if err != nil {
+		fmt.Println("Error instantiating cloudinary client")
+		return
+	}
+
+	resp, err := cld.Admin.DeleteAllAssets(context.Background(), admin.DeleteAllAssetsParams{})
+	if err != nil {
+		fmt.Println("Deletion failed to complete: ", err)
+	}
+
+	// Log the delivery URL
+	fmt.Println("****2. Deleted a folder****\nDelivery URL:", resp.Deleted, "\n")
+}
